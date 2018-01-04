43a8b3d091a95fac9fde999072af3fec07de9f9d
Rodolph Schwenzner <rschwenzner@gmail.com>
2018-01-04 22:37:48 +0400

    Setup production build and server

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,13 @@ const Header = () => (
   <header>
     <h1>Expensify</h1>
     <NavLink to="/" exact={true} activeClassName="is-active">Dashboard</NavLink>
-    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-    <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
+    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>  
     <NavLink to="/help" activeClassName="is-active">Help</NavLink>
   </header>
 )
 
 export default Header
+
+/*
+<NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
+*/
